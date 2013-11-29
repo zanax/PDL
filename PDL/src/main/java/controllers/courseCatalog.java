fcecbd60cbe9b776ed54f09fb0f816149345a5a8
gijsdewit <gijs.de.wit94@gmail.com>
2013-11-29 13:02:51 +0100

    Course overzicht en myCourses met goede layout

---
+++ PDL/src/main/java/controllers/courseCatalog.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+/**
+ *
+ * @author Niels
+ */
+import connection.DB;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.Course;
+import models.User;
+
+/**
+ *
+ * @author Zanax & Donna
+ */
+@WebServlet(name = "courseCatalog", urlPatterns = {"/courseCatalog"})
+public class courseCatalog extends HttpServlet {
+
+    public courseCatalog() {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false);
+        User user = (User) session.getAttribute("user");
+
+        List<Course> courses = DB.getInstance().getCourses();
+
+        request.setAttribute("courses", courses);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/pages/courseCatalog.jsp");
+        rd.forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+}
--- PDL/src/main/webapp/pages/courseCatalog.jsp
+++ PDL/src/main/webapp/pages/courseCatalog.jsp
@@ -1,6 +1,6 @@
 <%@include file="/includes/header.jsp" %>       
 <div class="content-header">
-    My courses
+    Courses
 </div>
 
 <c:forEach var="course" items="${courses}">
@@ -16,10 +16,13 @@
             <div id="course-content-description">
                 ${course.description}
             </div>
-            <div class="course-button info">
-                <a href="course_economics.html" class="button" id="button">Go to course</a>
+           <div class="course-button info">
+                <a href="course_economics.html" class="button" id="button">Enroll</a>
             </div>
-
+            <div class="course-button info">
+                <a href="course_economics.html" class="button" id="button">More info</a>
+                <a href="course_economics.html" class="button" id="button">Enroll</a>
+            </div>
         </div>
     </div>
 </c:forEach>
